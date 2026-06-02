87ab70e9b02597993087a4c2b073f2283d9ffd8a
Justine Gounga <justine.gounga@gmail.com>
2026-06-02 16:35:21 -0400

    added variables and output selector

---
+++ labs/lab-2/missionDecisionEngine.js
@@ -0,0 +1,11 @@
+
+//select the output paragraph
+const output = document.querySelector('#missionStatus');
+
+//mission variables
+let health = 65;
+let ammo = 8;
+let shield = true;
+let missionProgress = 40;
+let enemyNearby = true;
+

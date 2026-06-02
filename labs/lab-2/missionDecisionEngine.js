228b1556e33d8b6bccf1dd231ee3cbd355e32f2a
Justine Gounga <justine.gounga@gmail.com>
2026-06-02 16:41:30 -0400

    added critical failure condition

--- labs/lab-2/missionDecisionEngine.js
+++ labs/lab-2/missionDecisionEngine.js
@@ -9,3 +9,20 @@ let shield = true;
 let missionProgress = 40;
 let enemyNearby = true;
 
+//CONDITIONAL STATEMENTS
+
+//critical failure
+if (health < 30 && enemyNearby === true) 
+    {
+        output.textContent = 'CRITICAL ALERT: Immediate Evacuation Required';
+    }
+
+//low resource warning
+
+//mid mission status
+
+//high progress status
+
+//mission complete
+
+//default state

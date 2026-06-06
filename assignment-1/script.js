11b0ea36d68356e06f70ce6f6cb7b440674fad9c
Justine Gounga <justine.gounga@gmail.com>
2026-06-06 11:14:41 -0400

    added mission options arrays

---
+++ assignment-1/script.js
@@ -0,0 +1,15 @@
+
+// mission agent types array
+const agentTypes = ["Spy","Hacker","Scout", "Assassin", "Infiltrator"];
+
+// mission location array
+const missionLocations = ["Abandoned Warehouse", "Underground Bunker", "Enemy Headquarters", "Secret Lab", "Space Station"];
+
+// mission weapons array
+const weaponLoadouts = ["Pistol", "EMP Device", "Drones", "Snipper Rifle", "Katar"];
+
+// mission objectives array
+const missionObjectives = ["Gather Intel", "Rescue Hostages", "Collect Secret Files", "Disable Security System", "Recover Stolen Artifact"];
+
+// mission risk level array
+const riskLevels = ["Low", "Medium", "High", "Extreme"];
